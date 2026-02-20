7f81b497d4796911ee3d47f1904bfc36f358396a
sayam3004 <mutant.sayam>
2026-02-20 16:52:25 +0600

    Allow Origin

--- config/cors.php
+++ config/cors.php
@@ -17,20 +17,18 @@ return [
 
     'paths' => ['api/*','*'],
 
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    'allowed_methods' => ['*'],
 
     'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => [
-        'Content-Type', 'Authorization', 'X-localization', 'Accept',
-    ],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-   'supports_credentials' => true,
+   'supports_credentials' => false,
 
 ];
