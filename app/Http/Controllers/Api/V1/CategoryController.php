14b98365d97755805f89a515b1eee354ea3afc20
sayam3004 <mutant.sayam>
2026-03-02 04:06:02 +0600

    changes

--- app/Http/Controllers/Api/V1/CategoryController.php
+++ app/Http/Controllers/Api/V1/CategoryController.php
@@ -13,12 +13,12 @@ use Illuminate\Support\Facades\Validator;
 class CategoryController extends Controller
 {
 
-    public function get_categories(Request $request,$search=null)
+    public function get_categories(Request $request, $search = null)
     {
         try {
-        $category_list_default_status =Helpers::get_business_settings('category_list_default_status') ?? 1;
-        $category_list_sort_by_general = Helpers::getPriorityList(name: 'category_list_sort_by_general', type: 'general');
-            $zone_id=  $request->header('zoneId') ? json_decode($request->header('zoneId'), true) : [];
+            $category_list_default_status = Helpers::get_business_settings('category_list_default_status') ?? 1;
+            $category_list_sort_by_general = Helpers::getPriorityList(name: 'category_list_sort_by_general', type: 'general');
+            $zone_id =  $request->header('zoneId') ? json_decode($request->header('zoneId'), true) : [];
             $zoneIds = implode(',', $zone_id);
             $key = explode(' ', $search);
             $featured = $request->query('featured');
@@ -39,55 +39,57 @@ class CategoryController extends Controller
             //             ) AS total_order_count
             //         ")
 
-            $categories = Category:: with(['childes' => function($query)  {
-                $query->where('status',1)->select('id','name','image','slug','parent_id') ;
+            $categories = Category::with(['childes' => function ($query) {
+                $query->where('status', 1)->whereHas('module', function ($q) {
+                    $q->where('status', 1);
+                })->select('id', 'name', 'image', 'slug', 'parent_id');
             }]);
 
 
-            if($category_list_default_status != 1 && $category_list_sort_by_general == 'order_count' ){
-                $moduleCondition = config('module.current_module_data') ? 'AND items.module_id = '.(int)config('module.current_module_data')['id'] : '';
-                $categories= $categories->select('categories.*')
-             ->selectRaw("(SELECT COALESCE(SUM(items.order_count), 0) FROM items
+            if ($category_list_default_status != 1 && $category_list_sort_by_general == 'order_count') {
+                $moduleCondition = config('module.current_module_data') ? 'AND items.module_id = ' . (int)config('module.current_module_data')['id'] : '';
+                $categories = $categories->select('categories.*')
+                    ->selectRaw("(SELECT COALESCE(SUM(items.order_count), 0) FROM items
                             JOIN stores ON stores.id = items.store_id
                             WHERE items.is_approved = 1
                                 AND JSON_CONTAINS(items.category_ids, JSON_OBJECT('id', CAST(categories.id AS CHAR)), '$')
                                 AND JSON_CONTAINS(items.category_ids, JSON_OBJECT('position', 1), '$')
                                 AND stores.zone_id IN ($zoneIds)  $moduleCondition ) AS total_order_count");
-            }else{
-              $categories= $categories->select('id','name','image','slug');
+            } else {
+                $categories = $categories->select('id', 'name', 'image', 'slug');
             }
 
-             $categories= $categories->where(['position'=>0,'status'=>1])
-            ->when(config('module.current_module_data'), function($query){
-                $query->module(config('module.current_module_data')['id']);
-            })
-            ->when($featured, function($query){
-                $query->featured();
-            })
-            ->when($search, function($query)use($key){
-                $query->where(function ($q) use ($key) {
-                    foreach ($key as $value) {
-                        $q->orWhere('name', 'like', "%". $value."%");
-                    }
+            $categories = $categories->where(['position' => 0, 'status' => 1])
+                ->when(config('module.current_module_data'), function ($query) {
+                    $query->module(config('module.current_module_data')['id']);
+                })
+                ->when($featured, function ($query) {
+                    $query->featured();
+                })
+                ->when($search, function ($query) use ($key) {
+                    $query->where(function ($q) use ($key) {
+                        foreach ($key as $value) {
+                            $q->orWhere('name', 'like', "%" . $value . "%");
+                        }
+                    });
                 });
-            });
 
-            if($category_list_default_status  == 1){
-                $categories= $categories->orderBy('priority','desc');
+            if ($category_list_default_status  == 1) {
+                $categories = $categories->orderBy('priority', 'desc');
             } else {
                 $categories = match ($category_list_sort_by_general) {
-                        'latest' =>$categories->latest(),
-                        'oldest' => $categories->oldest(),
-                        'a_to_z' =>  $categories->orderby('name'),
-                        'z_to_a' =>  $categories->orderby('name','desc'),
-                        'order_count' =>  $categories->orderByDesc('total_order_count'),
-                         default => $categories,
-                    };
+                    'latest' => $categories->latest(),
+                    'oldest' => $categories->oldest(),
+                    'a_to_z' =>  $categories->orderby('name'),
+                    'z_to_a' =>  $categories->orderby('name', 'desc'),
+                    'order_count' =>  $categories->orderByDesc('total_order_count'),
+                    default => $categories,
+                };
             }
 
 
-        $categories = $categories->get()
-            ->map(function ($category) {
+            $categories = $categories->get()
+                ->map(function ($category) {
                     return [
                         'id' => $category->id,
                         'name' => $category->name,
@@ -114,7 +116,7 @@ class CategoryController extends Controller
     public function get_childes($id)
     {
         try {
-            $categories = Category::with('parent')->where(['parent_id' => $id,'status'=>1])->orderBy('priority','desc')->get();
+            $categories = Category::with('parent')->where(['parent_id' => $id, 'status' => 1])->orderBy('priority', 'desc')->get();
             return response()->json($categories, 200);
         } catch (\Exception $e) {
             return response()->json([], 200);
@@ -139,12 +141,12 @@ class CategoryController extends Controller
             return response()->json(['errors' => Helpers::error_processor($validator)], 403);
         }
 
-        $zone_id= $request->header('zoneId');
+        $zone_id = $request->header('zoneId');
 
         $type = $request->query('type', 'all');
 
         $data = CategoryLogic::products($id, $zone_id, $request['limit'], $request['offset'], $type);
-        $data['products'] = Helpers::product_data_formatting($data['products'] , true, false, app()->getLocale());
+        $data['products'] = Helpers::product_data_formatting($data['products'], true, false, app()->getLocale());
         return response()->json($data, 200);
     }
 
@@ -167,13 +169,13 @@ class CategoryController extends Controller
             return response()->json(['errors' => Helpers::error_processor($validator)], 403);
         }
 
-        $zone_id= $request->header('zoneId');
+        $zone_id = $request->header('zoneId');
 
         $type = $request->query('type', 'all');
-        $category_ids = $request['category_ids']?json_decode($request['category_ids']):'';
+        $category_ids = $request['category_ids'] ? json_decode($request['category_ids']) : '';
 
         $data = CategoryLogic::category_products($category_ids, $zone_id, $request['limit'], $request['offset'], $type);
-        $data['products'] = Helpers::product_data_formatting($data['products'] , true, false, app()->getLocale());
+        $data['products'] = Helpers::product_data_formatting($data['products'], true, false, app()->getLocale());
         return response()->json($data, 200);
     }
 
@@ -196,13 +198,13 @@ class CategoryController extends Controller
             return response()->json(['errors' => Helpers::error_processor($validator)], 403);
         }
 
-        $zone_id= $request->header('zoneId');
-        $longitude= $request->header('longitude');
-        $latitude= $request->header('latitude');
+        $zone_id = $request->header('zoneId');
+        $longitude = $request->header('longitude');
+        $latitude = $request->header('latitude');
         $type = $request->query('type', 'all');
 
-        $data = CategoryLogic::stores($id, $zone_id, $request['limit'], $request['offset'], $type,$longitude,$latitude);
-        $data['stores'] = Helpers::store_data_formatting($data['stores'] , true);
+        $data = CategoryLogic::stores($id, $zone_id, $request['limit'], $request['offset'], $type, $longitude, $latitude);
+        $data['stores'] = Helpers::store_data_formatting($data['stores'], true);
         return response()->json($data, 200);
     }
 
@@ -225,20 +227,20 @@ class CategoryController extends Controller
             return response()->json(['errors' => Helpers::error_processor($validator)], 403);
         }
 
-        $zone_id= $request->header('zoneId');
-        $longitude= $request->header('longitude');
-        $latitude= $request->header('latitude');
+        $zone_id = $request->header('zoneId');
+        $longitude = $request->header('longitude');
+        $latitude = $request->header('latitude');
         $type = $request->query('type', 'all');
-        $category_ids = $request['category_ids']?json_decode($request['category_ids']):'';
+        $category_ids = $request['category_ids'] ? json_decode($request['category_ids']) : '';
 
-        $data = CategoryLogic::category_stores($category_ids, $zone_id, $request['limit'], $request['offset'], $type,$longitude,$latitude);
-        $data['stores'] = Helpers::store_data_formatting($data['stores'] , true);
+        $data = CategoryLogic::category_stores($category_ids, $zone_id, $request['limit'], $request['offset'], $type, $longitude, $latitude);
+        $data['stores'] = Helpers::store_data_formatting($data['stores'], true);
         return response()->json($data, 200);
     }
 
 
 
-    public function get_all_products($id,Request $request)
+    public function get_all_products($id, Request $request)
     {
         if (!$request->hasHeader('zoneId')) {
             $errors = [];
@@ -247,7 +249,7 @@ class CategoryController extends Controller
                 'errors' => $errors
             ], 403);
         }
-        $zone_id= $request->header('zoneId');
+        $zone_id = $request->header('zoneId');
 
         try {
             return response()->json(Helpers::product_data_formatting(CategoryLogic::all_products($id, $zone_id), true, false, app()->getLocale()), 200);
@@ -274,28 +276,29 @@ class CategoryController extends Controller
             return response()->json(['errors' => Helpers::error_processor($validator)], 403);
         }
 
-        $zone_id= $request->header('zoneId');
+        $zone_id = $request->header('zoneId');
 
         $type = $request->query('type', 'all');
 
         $data = CategoryLogic::featured_category_products($zone_id, $request['limit'], $request['offset'], $type);
-        $data['products'] = Helpers::product_data_formatting($data['products'] , true, false, app()->getLocale());
+        $data['products'] = Helpers::product_data_formatting($data['products'], true, false, app()->getLocale());
         return response()->json($data, 200);
     }
 
-    public function get_popular_category_list(){
+    public function get_popular_category_list()
+    {
 
-        $avg_items=Item::where('order_count','>=', 1 )->avg('order_count') ?? 0;
+        $avg_items = Item::where('order_count', '>=', 1)->avg('order_count') ?? 0;
 
-        $items= Item::where('order_count','>', $avg_items )->pluck('category_ids');
-        $get_popular_category_ids = $items->flatMap(function($categoryIds) {
+        $items = Item::where('order_count', '>', $avg_items)->pluck('category_ids');
+        $get_popular_category_ids = $items->flatMap(function ($categoryIds) {
             $categories = json_decode($categoryIds, true);
-                return collect($categories)->pluck('id');
-            })->unique();
-        $categories= Category::when(config('module.current_module_data'), function($query){
+            return collect($categories)->pluck('id');
+        })->unique();
+        $categories = Category::when(config('module.current_module_data'), function ($query) {
             $query->module(config('module.current_module_data')['id']);
         })
-        ->whereIn('id',$get_popular_category_ids->toArray())->where(['position'=>0,'status'=>1])->take(20)->get();
+            ->whereIn('id', $get_popular_category_ids->toArray())->where(['position' => 0, 'status' => 1])->take(20)->get();
         return response()->json($categories, 200);
     }
 }
