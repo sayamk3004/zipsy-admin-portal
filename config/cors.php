42646306a765aa55566ceb57d454754b882d9acf
sayam3004 <mutant.sayam>
2026-03-08 17:28:33 +0600

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://zipsy.co.uk','http://localhost:3000'],
+    'allowed_origins' => [
+        'https://zipsy.co.uk',
+        'https://admin.zipsy.co.uk',
+        'http://localhost:3000',
+        'http://localhost:5173',
+    ],
 
     'allowed_origins_patterns' => [],
 
